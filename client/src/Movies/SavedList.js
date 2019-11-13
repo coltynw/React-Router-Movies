8f04544c07b6b826116097d5bde95412baf2db3d
coltynw <coltynw@users.github.com>
2019-11-12 19:02:48 -0500

    coltyn

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SavedList = props => (
-  <div className="saved-list">
+  <div className='saved-list'>
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+      <Link to={`/Movies/${movie.id}`}>
+        <span className='saved-movie'>{movie.title}</span>
+      </Link>
     ))}
-    <div className="home-button">Home</div>
+    <Link className='home-button' to='s/'>
+      Home
+    </Link>
   </div>
 );
 
